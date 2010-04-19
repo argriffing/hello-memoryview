da223d51bb2245234b818d794ca543e079a40f0c
Foo Bar <argriffi@qsl.nc.rr.com>
2010-04-18 23:03:03 -0400

    remove the build directory

--- build/scripts-2.6/hello-memoryview.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-
-def main(args):
-    pass
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__)
-    args = parser.parse_args()
-    main(args)
--- build/scripts-2.7/hello-memoryview.py
+++
@@ -1,9 +0,0 @@
-#!/home/argriffi/virtual/e27/bin/python
-
-def main(args):
-    pass
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description=__doc__)
-    args = parser.parse_args()
-    main(args)
