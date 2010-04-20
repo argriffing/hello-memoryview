3ddd012cc3ac870f1c11a38b173628e9c0425a83
Foo Bar <argriffi@qsl.nc.rr.com>
2010-04-20 16:41:40 -0400

    the C extension now works

--- bin/hello-memoryview.py
+++ bin/hello-memoryview.py
@@ -1,9 +1,95 @@
 #! /bin/env python
 
-def main(args):
-    pass
+import argparse
+import curses
+import time
+import random
+
+import numpy as np
+
+import life
+
+def gen_offsets():
+    for i in (-1, 0, 1):
+        for j in (-1, 0, 1):
+            if i or j:
+                yield i, j
+
+def lifestub(curr, next):
+    """
+    Implement Conway Life rules.
+    A cell is born if it has exactly 3 neighbours,
+    stays alive if it has 2 or 3 living neighbours, and dies otherwise.
+    @param curr: numpy 2d int array (will not be modified)
+    @param next: numpy 2d int array (will be modified)
+    """
+    nrows, ncols = curr.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            value = curr[i, j]
+            nneighbors = 0
+            for di, dj in gen_offsets():
+                ni = (i + di) % nrows
+                nj = (j + dj) % ncols
+                nneighbors += curr[ni, nj]
+            if value and nneighbors in (2, 3):
+                next[i, j] = 1
+            elif (not value) and nneighbors == 3:
+                next[i, j] = 1
+            else:
+                next[i, j] = 0
+
+def draw_board(scr, board):
+    nrows, ncols = board.shape
+    for i in range(nrows):
+        for j in range(ncols):
+            c = 'O' if board[i, j] else '.'
+            # A benign exception is raised when you draw on the last
+            # character of a row, so ignore the exception.
+            try:
+                scr.addstr(i, j, c)
+            except curses.error, e:
+                pass
+    scr.refresh()
+
+def main(stdscr, args):
+    # get the screen size
+    nrows, ncols = stdscr.getmaxyx()
+    # initialize the game of life board
+    board_a = np.array([
+        [random.randrange(2) for i in range(ncols)]
+        for j in range(nrows)])
+    # this is the back buffer for updating the board
+    board_b = board_a.copy()
+    # draw the board
+    draw_board(stdscr, board_a)
+    # do the main loop
+    countbuffer = []
+    while True:
+        c = stdscr.getkey()
+        if c == 'n':
+            count = 1
+            if countbuffer:
+                count = int(''.join(countbuffer))
+                countbuffer = []
+            for i in range(count):
+                # go to the next generation
+                if args.fast:
+                    life.step(board_a, board_b)
+                else:
+                    lifestub(board_a, board_b)
+                # flip the boards
+                board_a, board_b = board_b, board_a
+            # draw the updated board
+            draw_board(stdscr, board_a)
+        elif c.isdigit():
+            countbuffer.append(c)
+        else:
+            return
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('--fast', action='store_true',
+            help='use a C extension for speed')
     args = parser.parse_args()
-    main(args)
+    curses.wrapper(main, args)
--- lifemodule.c
+++ lifemodule.c
@@ -2,34 +2,127 @@
 
 static PyObject *LifeError;
 
+/* One generation of Conway Life.
+ */
+int do_one_generation(long *source, long *sink, int nrows, int ncols)
+{
+  int row, col;
+  int dr, dc;
+  for (row=0; row<nrows; ++row) {
+    for (col=0; col<ncols; ++col) {
+      /* get the source value and the number of neighbors for this position */
+      int index = row * ncols + col;
+      int value = source[index];
+      int nneighbors = 0;
+      for (dr=-1; dr<2; ++dr) {
+        for (dc=-1; dc<2; ++dc) {
+          if (dr || dc) {
+            int r = row + dr;
+            int c = col + dc;
+            if (r == nrows) {
+              r = 0;
+            } else if (r == -1) {
+              r = nrows - 1;
+            }
+            if (c == ncols) {
+              c = 0;
+            } else if (c == -1) {
+              c = ncols -1;
+            }
+            int neighbor_index = r * ncols + c;
+            nneighbors += source[neighbor_index];
+          }
+        }
+      }
+      /* set the sink value according to the source value and the neighbors */
+      int next_value = 0;
+      if (value && (nneighbors == 2 || nneighbors == 3)) {
+        next_value = 1;
+      } else if (!value && nneighbors == 3) {
+        next_value = 1;
+      }
+      sink[index] = next_value;
+    }
+  }
+  return 0;
+}
+
 static PyObject *
 step_python (PyObject *self, PyObject *args)
 {
+  int except = 0;
   /* read the args */
   PyObject *obj_source;
   PyObject *obj_sink;
-  int ok = PyArg_ParseTuple(args, "OO", &obj_source, &obj_sink);
-  if (!ok) goto end;
-  /* read the vector of likelihoods */
-  /*
-  Py_ssize_t nlikelihoods;
-  double *l_big = double_vector_helper(ltuple, &nlikelihoods);
-  if (!l_big)
-  {
-    PyErr_SetString(HmmuscError, "likelihood init error");
-    goto end;
+  if (!PyArg_ParseTuple(args, "OO", &obj_source, &obj_sink)) {
+    except = 1; goto end;
   }
-  */
-  /*
-  dtuple = PyTuple_New(nlikelihoods);
-  Py_ssize_t i;
-  for (i=0; i<nlikelihoods; i++)
-  {
-    PyTuple_SetItem(dtuple, i, PyFloat_FromDouble(d_big[i]));
+  /* both objects should support the buffer interface */
+  if (!PyObject_CheckBuffer(obj_source)) {
+    PyErr_SetString(LifeError,
+        "the source object does not support the buffer interface");
+    except = 1; goto end;
   }
-  */
+  if (!PyObject_CheckBuffer(obj_sink)) {
+    PyErr_SetString(LifeError,
+        "the sink object does not support the buffer interface");
+    except = 1; goto end;
+  }
+  /* get the buffer view for each object */
+  Py_buffer source_view;
+  Py_buffer sink_view;
+  int flags = PyBUF_ND | PyBUF_FORMAT | PyBUF_C_CONTIGUOUS | PyBUF_WRITABLE;
+  if (PyObject_GetBuffer(obj_source, &source_view, flags) < 0) {
+    except = 1; goto end;
+  }
+  if (PyObject_GetBuffer(obj_sink, &sink_view, flags) < 0) {
+    except = 1; goto end;
+  }
+  /* each buffer should be two dimensional */
+  if (source_view.ndim != 2) {
+    PyErr_SetString(LifeError, "the source buffer should be 2D");
+    except = 1; goto end;
+  }
+  if (sink_view.ndim != 2) {
+    PyErr_SetString(LifeError, "the sink buffer should be 2D");
+    except = 1; goto end;
+  }
+  /* the buffers should be the same shape */
+  if (source_view.shape[0] != sink_view.shape[0] ||
+      source_view.shape[1] != sink_view.shape[1]) {
+    PyErr_SetString(LifeError,
+        "the source and sink buffers should be the same shape");
+    except = 1; goto end;
+  }
+  /* format should be long (l) */
+  if (source_view.format[0] != 'l') {
+    PyErr_SetString(LifeError, "source buffer format should be long");
+    except = 1; goto end;
+  }
+  if (sink_view.format[0] != 'l') {
+    PyErr_SetString(LifeError, "sink buffer format should be long");
+    except = 1; goto end;
+  }
+  /* buffers should be non-null */
+  if (source_view.buf == NULL) {
+    PyErr_SetString(LifeError, "source buffer is NULL");
+    except = 1; goto end;
+  }
+  if (sink_view.buf == NULL) {
+    PyErr_SetString(LifeError, "sink buffer is NULL");
+    except = 1; goto end;
+  }
+  /* get the number of rows and columns */
+  int nrows = source_view.shape[0];
+  int ncols = source_view.shape[1];
+  /* do a generation of conway life */
+  do_one_generation(source_view.buf, sink_view.buf, nrows, ncols);
 end:
-  return Py_BuildValue("i", 42);
+  if (except) {
+    return NULL;
+  } else {
+    return Py_BuildValue("i", 42);
+  }
 }
 
 static PyMethodDef LifeMethods[] = {
